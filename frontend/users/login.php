8d761c06cf30ef30fe2abf84bf04161de944179c
Sighlesh <sylusperez@gmail.com>
2025-04-09 21:48:12 -0400

    Modified button styling at the end of pages. Made changes to css stylings

--- frontend/users/login.php
+++ frontend/users/login.php
@@ -19,7 +19,7 @@
         <label for="password">Password:</label>
         <input type="password" id="password" name="password" required><br><br>
 
-        <input type="submit" value="Login">
+        <input class="button" type="submit" value="Login">
     </form>
 
     <script type="module" src="login.js"></script>
--- frontend/users/register.php
+++ frontend/users/register.php
@@ -11,8 +11,8 @@
 </header>
 
 <body>
-    <h1>Register Account</h1>
     <form id="registerForm">
+        <h1>Register Account</h1>
         <label for="firstName">First Name:</label>
         <input type="text" id="firstName" name="firstName" required><br><br>
 
@@ -31,12 +31,13 @@
         <label for="confirmPassword">Confirm Password:</label>
         <input type="password" id="confirmPassword" name="confirmPassword" required><br><br>
 
-        <input type="submit" value="Register">
+        <input class="button" type="submit" value="Register">
     </form>
-
-    <button class="button" onclick="window.location.href='../users/universityCreation.php';">
-        Register University and Super-Admin
-    </button>
+    <div class="button-container">
+        <button class="button" onclick="window.location.href='../users/universityCreation.php';">
+            Register University and Super-Admin
+        </button>
+    </div>
 
     <script src="register.js"></script>
     <?php include '../templates/errorModal.php';?>
